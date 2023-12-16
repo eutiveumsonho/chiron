b99b69fde7e55fdfb355a1cb9638cb9fc876186d
marcelovicentegc <marcelovicentegc@gmail.com>
2023-12-16 10:28:31 -0300

    docs(gen): update idx manager

--- index_manager.sh
+++ index_manager.sh
@@ -5,19 +5,6 @@ find_first_html_file () {
     find "$1" -name '*.html' | head -n 1
 }
 
-# This script will add a new line of HTML to the index.html file
-# The new line of HTML will be added after the line that contains the comment <!-- @jsdoc-builder-action new-version -->
-update_index_html () {
-    awk -v line="$2" '
-        /<!-- @jsdoc-builder-action new-version -->/ {
-            print
-            print "      "line""
-            next
-        }
-        {print}
-    ' "$1" > tmp_index.html && mv tmp_index.html "$1"
-}
-
 # This script will create an index.html file in the specified directory in case it doesn't exist
 create_index_html () {
     if [ ! -f "$1/index.html" ]; then
@@ -26,6 +13,26 @@ create_index_html () {
     else
         echo "index.html exists in $1"
     fi
+}
+
+# This script will add a new line of HTML to the index.html file
+# The new line of HTML will be added after the line that contains the comment <!-- @jsdoc-builder-action new-version -->
+update_index_html () {
+    # If file already contains the input, skip it
+    grep -q "$2" "$1"
+    if [ $? -eq 0 ]; then
+        echo "File already contains $2"
+        return
+    fi
+
+    awk -v line="$2" '
+        /<!-- @jsdoc-builder-action new-version -->/ {
+            print
+            print "      "line""
+            next
+        }
+        {print}
+    ' "$1" > tmp_index.html && mv tmp_index.html "$1"
 }
 
 export find_first_html_file
